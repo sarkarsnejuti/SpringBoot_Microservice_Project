38eb1c77fa5bfb44ed1d5846d5727abd27db7b48
sarkarsnejuti <snejuti2019@gmail.com>
2026-08-07 07:39:08 +0530

    Second Commit

--- accounts/src/main/java/Microservice/accounts/controller/AccountsController.java
+++ accounts/src/main/java/Microservice/accounts/controller/AccountsController.java
@@ -2,6 +2,7 @@ package Microservice.accounts.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,32 +16,43 @@ import Microservice.accounts.constants.AccountsCostants;
 import Microservice.accounts.dto.CustomerDto;
 import Microservice.accounts.dto.ResponseDto;
 import Microservice.accounts.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 
+
+@Tag(name = "CURD REST APIs for Accounts in EazyBank")
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
+@Validated
 public class AccountsController {
 	
 	private IAccountsService iAccountsService;
 	
+	
+	@Operation(summary = "Create Account for REST Api")
 	@PostMapping("/create")
-	public ResponseEntity<ResponseDto>createAccount(@RequestBody CustomerDto customerDto){
+	public ResponseEntity<ResponseDto>createAccount(@Valid @RequestBody CustomerDto customerDto){
 		
 		iAccountsService.createAccount(customerDto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(AccountsCostants.STATUS_201, AccountsCostants.MESSAGE_201));
 		
 	}
 	
+	@Operation(summary = "Fetch user details of EazyBank")
 	@GetMapping("/fetch")
-	public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber){
+	public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile num must be 10 digits") String mobileNumber){
 		
 		CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
 		return ResponseEntity.status(HttpStatus.OK).body(customerDto);
 	}
 
+	@Operation(summary = "Update the user details of EazyBank")
 	@PutMapping("/update")
-	public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto){
+	public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto){
 		
 		boolean isUpdated = iAccountsService.updateAccount(customerDto);
 		
@@ -51,8 +63,9 @@ public class AccountsController {
 		}
 	}
 	
+	@Operation(summary = "Delete the User details of EazyBank")
 	@DeleteMapping("/delete")
-	public ResponseEntity<ResponseDto>deleteAccountDetails(@RequestParam String mobileNumber){
+	public ResponseEntity<ResponseDto>deleteAccountDetails(@RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile num must be 10 digits") String mobileNumber){
 		boolean isDelete = iAccountsService.deleteAccount(mobileNumber);
 		
 		if(isDelete) {
--- accounts/src/main/java/Microservice/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/Microservice/accounts/service/impl/AccountsServiceImpl.java
@@ -36,8 +36,10 @@ public class AccountsServiceImpl implements IAccountsService{
 			
 			throw new CustomerAlreadyExistsException("Customer already registered with the given mobile no" +customerDto.getMobileNumber());
 		}
-		customer.setCreatedAt(LocalDateTime.now());
-		customer.setCreatedBy("Anonymous");
+		/*
+		 * customer.setCreatedAt(LocalDateTime.now());
+		 * customer.setCreatedBy("Anonymous");
+		 */
 		Customer savedCustomer = customerRepository.save(customer);
 		accountsRepository.save(createNewAccount(savedCustomer));
 		
@@ -54,9 +56,10 @@ public class AccountsServiceImpl implements IAccountsService{
 		newAccount.setAccountNumber(accountNo);
 		newAccount.setAccountType(AccountsCostants.SAVINGS);
 		newAccount.setBranchAddress(AccountsCostants.ADDRESS);
-		newAccount.setCreatedAt(LocalDateTime.now());
-		newAccount.setCreatedBy("Anonymous");
-		
+		/*
+		 * newAccount.setCreatedAt(LocalDateTime.now());
+		 * newAccount.setCreatedBy("Anonymous");
+		 */
 		return newAccount;
 		
 	}
